db69a1f10be83335807dd5d291c78d5589d85633
Omar <omar@na2.com.br>
2022-01-08 12:20:24 -0300

    fixed package.json and hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,8 @@
 import * as dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import "@nomiclabs/hardhat-ethers";
 
 dotenv.config();
 
@@ -25,13 +22,6 @@ const config: HardhatUserConfig = {
 			accounts:
 				process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
 		},
-	},
-	gasReporter: {
-		enabled: process.env.REPORT_GAS !== undefined,
-		currency: "USD",
-	},
-	etherscan: {
-		apiKey: process.env.ETHERSCAN_API_KEY,
 	},
 };
 
