dd516c3d1e43cead2415f7cb6bf1dcd97f21595b
Omar <omar@na2.com.br>
2022-01-08 11:48:19 -0300

    bumped solidity to 0.8.11

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,29 +10,29 @@ import "solidity-coverage";
 dotenv.config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+	const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+	for (const account of accounts) {
+		console.log(account.address);
+	}
 });
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-  networks: {
-    rinkeby: {
-      url: process.env.RINKEBY_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+	solidity: "0.8.11",
+	networks: {
+		rinkeby: {
+			url: process.env.RINKEBY_URL || "",
+			accounts:
+				process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+		},
+	},
+	gasReporter: {
+		enabled: process.env.REPORT_GAS !== undefined,
+		currency: "USD",
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY,
+	},
 };
 
 export default config;
